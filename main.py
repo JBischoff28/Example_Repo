c56e9a3da4ca99ae159c5535d6b1792f35a516b9
Joshua Bischoff <joshuabischoff1@gmail.com>
2022-10-01 16:56:21 -0400

    Created main.py file to practice pushing files to GitHub.

---
+++ main.py
@@ -0,0 +1,6 @@
+user_first_name = input("What is your first name?\n")
+user_last_name = input("What is your last name?\n")
+
+user_full_name = user_first_name + " " + user_last_name
+
+print(f"So your full name is {user_full_name}?\n")
