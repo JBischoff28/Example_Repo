e684bee3fde9a44c7806cc2db8c568296705ef08
Joshua Bischoff <joshuabischoff1@gmail.com>
2022-10-01 21:40:38 -0400

    Added a very simple number guessing game to main.py.

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ user_last_name = input("What is your last name?\n")
 user_full_name = user_first_name + " " + user_last_name
 
 print(f"So your full name is {user_full_name}?\n")
+
+print("The code from here on is done from my desktop!\n")
+
+num = input("Guess a number 1-10!\n")
+correct_num = 8
+
+if num == "8":
+    print("That's right!\n")
+else:
+    print("Nope!\n")
